81afde24b9653ed579846cff71c96377fcca143f
Zakaria ayad <109592022+AyadZakaria@users.noreply.github.com>
2023-09-11 16:42:33 +0100

    Update index.js

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ const port = process.env.PORT || 3001;
 
 // Middleware
 app.use(cors({
-  origin:["https://ayad-api.vercel.app/"],
+  origin:["https://ayad-api.vercel.app"],
   methods:["POST", "GET"],
   credentials:true
 }));
